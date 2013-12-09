f471b2e25d0845a3e6edb527a5dd0dea862573c0
hendras <hendarkfuneral@gmail.com>
2013-12-09 19:47:06 +0700

    dasardasarphp
    dasarphp

---
+++ dasar2_php/lat1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml" xml : lang="en" lang="en">
+<head>
+	<title>Demo</title>
+</head>
+
+<body>
+
+<?php
+echo 'kode PHP di sini';
+// ...
+?>
+
+<p> Dokument HTML </p>
+
+<?php
+echo 'Kode PHP di sini';
+// ...
+?>
+
+</body>
+</html>
---
+++ dasar2_php/lat2.3.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Latihan Variabel</title>
+</head>
+
+<body>
+
+<?php 
+// Deklarasi dan inisiasi
+$bil = 3;
+var_dump($bil);
+// Outptt : int(3)
+
+$var= "";
+var_dump($var);
+// Output : string(0) ""
+
+$var= null;
+var_dump($var);
+// Output : NULL ""
+
+?>
+
+</body>
+</html>
